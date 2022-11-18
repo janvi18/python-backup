50f28d8466b945c49ccee57a5df2ec6d775f622d
janvi18 <sjanvi9333@gmail.com>
2022-11-19 00:01:45 +0530

    task

--- tasks.py
+++ tasks.py
@@ -1,12 +1,10 @@
 """# multi table
-
 n = int(input("Enter the number "))
 for i in range(1, 11):
     c = n*i
-    print(n, "*", i, "=", c)
-"""
+    print(n, "*", i, "=", c)"""
 
-# reverse
+"""# reverse
 num = 1234
 reversed_num = 0
 
@@ -15,4 +13,14 @@ while num != 0:
     reversed_num = reversed_num * 10 + digit
     num //= 10
 
-print("Reversed Number : " + str(reversed_num))
+print("Reversed Number : " + str(reversed_num))"""
+
+
+# sum of digits
+sum = 0
+num = int(input("Enter a number: "))
+while (num > 0):
+    x = num % 10
+    sum = sum + x
+    num = num // 10
+print("Sum of digits of the number is: ", sum)
